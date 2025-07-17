b3c6ce05ece32b466b34ccf9254c204958a7e372
Emerson Joel Pereira <150153374+NomeDecente@users.noreply.github.com>
2025-07-17 12:25:07 +0000

    Refatorar função de obtenção de dados para usar getStaticProps e melhorar a estrutura do componente Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,30 @@
 import { Container, Row } from "react-bootstrap";
 import Cards from "./components/cards";
-export async function getServerSideProps() {
-  // Fetch data from  API notícias
-  const res = await fetch('https://emerson5b.vercel.app/api/noticias/apinoticias')
-  const repo = await res.json()
-  // Pass data to the page via props
-  return { props: { noticias: repo } }
+
+export async function getStaticProps() {
+  const res = await fetch('https://emerson5b.vercel.app/api/noticias/apinoticias');
+  const repo = await res.json();
+  return { props: { noticias: repo } };
 }
+
 export default function Home({ noticias }) {
-  return <>
+  return (
     <Container>
       <Row xs={1} md={3}>
         Main index
-        {Array.isArray(noticias) ?
-          noticias.map(noticia => (
-            <Cards idnoticia={noticia.idnoticia}
-              conteudonoticia={noticia.conteudonoticia}
-              titulonoticia={noticia.titulonoticia}
-              datahoracadastro={noticia.datahoracadastro}
-              tiponoticia={noticia.tiponoticia} />
-          ))
+        {Array.isArray(noticias)
+          ? noticias.map(noticia => (
+              <Cards
+                key={noticia.idnoticia}
+                idnoticia={noticia.idnoticia}
+                conteudonoticia={noticia.conteudonoticia}
+                titulonoticia={noticia.titulonoticia}
+                datahoracadastro={noticia.datahoracadastro}
+                tiponoticia={noticia.tiponoticia}
+              />
+            ))
           : "não"}
       </Row>
     </Container>
-  </>
+  );
 }
